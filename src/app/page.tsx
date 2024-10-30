f9cd052abdff37f0cf04b99dad15410952c1fc4d
PurinPintakhiew <dev.purin@gmail.com>
2024-10-30 23:44:32 +0700

    chore : home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,17 @@
 export default function Home() {
   return (
-    <div className="">
-      <main></main>
+    <div className="container">
+      <main>
+        <div className="mt-5">
+          <div className="grid grid-cols-12">
+            <div className="col-span-12 md:col-span-3">Novel</div>
+            <div className="col-span-12 md:col-span-3">Manga</div>
+            <div className="col-span-12 md:col-span-3">History</div>
+            <div className="col-span-12 md:col-span-3">Philosophy</div>
+            <div className="col-span-12 md:col-span-3">Politics and Government</div>
+          </div>
+        </div>
+      </main>
     </div>
   );
 }
