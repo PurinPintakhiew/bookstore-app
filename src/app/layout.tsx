4ab7f88a7115eb3c0f1580ab81710a25e5851918
PurinPintakhiew <dev.purin@gmail.com>
2024-10-30 23:28:37 +0700

    chore : layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
-import "./globals.css";
+import "@/assets/css/globals.css";
+import MasterLayout from "@/layouts/MasterLayout";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -25,7 +26,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <MasterLayout>{children}</MasterLayout>
+      </body>
     </html>
   );
 }
