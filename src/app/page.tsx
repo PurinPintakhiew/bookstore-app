26fa298c291ddbb424d040b242707ba7a30dd997
PurinPintakhiew <dev.purin@gmail.com>
2024-11-02 20:26:36 +0700

    chore : use CategoryList

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,12 @@
+import CategoryList from "@/components/pages/home/CategoryList";
+import categories from "@/data/categories.json";
+
 export default function Home() {
   return (
     <div className="container">
       <main>
         <div className="mt-5">
-          <div className="grid grid-cols-12">
-            <div className="col-span-12 md:col-span-3">Novel</div>
-            <div className="col-span-12 md:col-span-3">Manga</div>
-            <div className="col-span-12 md:col-span-3">History</div>
-            <div className="col-span-12 md:col-span-3">Philosophy</div>
-            <div className="col-span-12 md:col-span-3">Politics and Government</div>
-          </div>
+          <CategoryList data={categories} />
         </div>
       </main>
     </div>
